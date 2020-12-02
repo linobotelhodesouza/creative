bbba7255e4524d6e7bbfaf58535d1d1a73bd7858
Lino Botelho de Souza <57472342+linobotelhodesouza@users.noreply.github.com>
2020-12-01 23:01:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var stream = document.querySelector('.gallery__stream');
+  var items = document.querySelectorAll('.gallery__item');
+  
+  var prev = document.querySelector('.gallery__prev');
+  prev.addEventListener('click', function() {
+    stream.insertBefore(items[items.length - 1], items[0]);
+    items = document.querySelectorAll('.gallery__item');
+  });
+  
+  var next = document.querySelector('.gallery__next');
+  next.addEventListener('click', function() {
+    stream.appendChild(items[0]);
+    items = document.querySelectorAll('.gallery__item');
+  });
+});
